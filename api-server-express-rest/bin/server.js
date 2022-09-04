d7045c31e0d40aa40e1881fed3ae76afacb0fb57
Fabricio N Pinheiro <fabricionpinheiro7@gmail.com>
2022-09-04 13:56:56 -0300

    initial commit.

---
+++ api-server-express-rest/bin/server.js
@@ -0,0 +1,5 @@
+const app = require("../src/app");
+
+app.listen(3033, () => {
+  console.log("API rodando com sucesso");
+});
---
+++ api-server-express-rest/src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const router = express.Router();
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+const routes = router.get("/get", (req, res) => {
+  res.status(200).send({
+    title: "Node Store API",
+    version: "0.0.1",
+  });
+});
+
+app.use("/", routes);
+
+module.exports = app;
