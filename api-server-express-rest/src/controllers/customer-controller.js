eae3c61e1dafee1d01f1cc5103fffedd114fa6db
Fabricio N Pinheiro <fabricionpinheiro7@gmail.com>
2022-09-24 11:58:14 -0300

    config: encriptando senha.

--- api-server-express-rest/src/controllers/customer-controller.js
+++ api-server-express-rest/src/controllers/customer-controller.js
@@ -1,8 +1,14 @@
+const md5 = require('md5');
 const repository = require('../repositories/customer-repository');
 
 exports.createNewCustomer = async (req, res) => {
+  const { name, email, password } = req.body;
   try {
-    const data = await repository.createNewCustomer(req.body);
+    const data = await repository.createNewCustomer({
+      name,
+      email,
+      password: md5(password),
+    });
     res.status(201).json(data);
   } catch (error) {
     res.status(500).send(error.message);
