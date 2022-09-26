f568a77318a1c80a4659a78d34585e1ab127ceaf
Fabricio N Pinheiro <fabricionpinheiro7@gmail.com>
2022-09-26 12:28:16 -0300

    feat: integração com serviço de envio de email sendgrid.

--- api-server-express-rest/src/controllers/customer-controller.js
+++ api-server-express-rest/src/controllers/customer-controller.js
@@ -1,5 +1,6 @@
 const md5 = require('md5');
 const repository = require('../repositories/customer-repository');
+const emailService = require('../services/email.service');
 
 exports.createNewCustomer = async (req, res) => {
   const { name, email, password } = req.body;
@@ -9,6 +10,13 @@ exports.createNewCustomer = async (req, res) => {
       email,
       password: md5(password),
     });
+
+    await emailService.send(
+      email,
+      'Bem vindo a Node Store',
+      process.env.EMAIL_TMPL.replace('{0}', name),
+    );
+
     res.status(201).json(data);
   } catch (error) {
     res.status(500).send(error.message);
