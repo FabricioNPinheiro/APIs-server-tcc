c871782746b4bf7a0619e68fee2354b53fa691a4
Fabricio N Pinheiro <fabricionpinheiro7@gmail.com>
2022-10-21 01:24:56 -0300

    config: alterando de banco de dados em nuvem para local.

--- api-server-express-rest/src/database/connect.js
+++ api-server-express-rest/src/database/connect.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
-  await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ecommerce-tcc.k6wjj5n.mongodb.net/?retryWrites=true&w=majority`, (error) => {
+  await mongoose.connect(`mongodb://localhost:27017/ecommerce`, (error) => {
     if (error) {
-      return console.log('Ocorreu um erro ao se conectar com o banco de dados: ', error);
+      return console.log(
+        "Ocorreu um erro ao se conectar com o banco de dados: ",
+        error
+      );
     }
-    return console.log('Conexão ao banco de dados realizada com sucesso!');
+    return console.log("Conexão ao banco de dados realizada com sucesso!");
   });
 };
 
